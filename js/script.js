abd91a7c237406385969a1980be935baab510462
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-02-15 22:42:45 -0500

    colorpicker

--- js/script.js
+++ js/script.js
@@ -4,8 +4,11 @@ window.addEventListener("load", () => {
     const age = document.getElementById("age");
     const color = document.getElementById("color");
     console.log(username.value, age.value, color.value);
-    if( username.value ) && (age.value) && (color.value)
-    window.location.href = "game.html";
+    if ((username.value === "") || (age.value === "") || (color.value === "")){
+      alert("Please make sure you have entered appropriate inputs");
+    } else {
+      window.location.href="game.html";
+    }
   });
 });
 
