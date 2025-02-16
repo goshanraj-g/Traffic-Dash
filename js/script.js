8fd8e477bad677ab7287340d8c2937e67f84c2eb
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-02-15 20:41:23 -0500

    finished homepage

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let username = document.getElementById("name");
+let age = document.getElementById("age");
+let color = document.getElementById("color");
+let go = document.getElementById("go");
+
+username.addEventListener("click",function() {
+    console.log(username);
+})
+
+age.addEventListener("click",function() {
+    console.log(age);
+})
+
+color.addEventListener("click",function() {
+    console.log(color);
+})
+
+go.addEventListener("click",function() {
+    console.log(go);
+})
