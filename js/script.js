519bc8136c0df2563a062e62559ba019d6971761
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-02-16 21:03:49 -0500

    movement

--- js/script.js
+++ js/script.js
@@ -1,18 +1,60 @@
 window.addEventListener("load", () => {
+  const go = document.getElementById("go");
+  const username = document.getElementById("name");
+  const age = document.getElementById("age");
+  const color = document.getElementById("color");
+  const body = document.body;
+  const car = document.getElementById("car");
+  let carX = 0;
+  let carY = 0;
+  const speed = 3;
+
+  document.querySelector(".game-page").style.display = "none";
+
   go.addEventListener("click", () => {
-    const username = document.getElementById("name");
-    const age = document.getElementById("age");
-    const color = document.getElementById("color");
-    console.log(username.value, age.value, color.value);
-    if ((username.value === "") || (age.value === "") || (color.value === "")){
+    if (username.value === "" || age.value === "" || color.value === "") {
       alert("Please make sure you have entered appropriate inputs");
     } else {
-      window.location.href="game.html";
+      document.querySelector(".intro-box").style.display = "none";
+
+      document.querySelector(".game-page").style.display = "flex";
+
+      body.style.backgroundImage = "none";
+      car.focus();
     }
   });
-});
 
+  const keys = {};
 
+  document.addEventListener("keydown", (event) =>{
+    keys[event.key] = true;
+  });
 
+  document.addEventListener("keyup", (event) => {
+    keys[event.key] = false;
+  })
 
+  function update() {
+    if (keys["ArrowUp"] || keys["w"] || keys["W"]) {
+      carY -= speed;
+    }
 
+    if (keys["ArrowLeft"] || keys["a"] || keys["A"]) {
+      carX -= speed;
+    }
+
+    if (keys["ArrowDown"] || keys["s"] || keys["S"]) {
+      carY += speed;
+    }
+
+    if (keys["ArrowRight"] || keys["d"] || keys["D"]) {
+      carX += speed;
+    }
+
+    car.style.transform = `translate(${carX}px, ${carY}px)`;
+
+    requestAnimationFrame(update);
+  }
+  requestAnimationFrame(update);
+
+  });;
